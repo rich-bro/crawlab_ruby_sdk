4b799fea6d7e4d2749ce4ba83305bd1c3d4c4420
李金敏 <lijinmin@socialdatamax.com>
2023-06-15 14:41:05 +0800

    m

---
+++ lib/client/oss_server_client.rb
@@ -0,0 +1,50 @@
+require 'aliyun/oss'
+
+class OssServerClient
+	attr_accessor oss_client
+	attr_accessor bucket
+
+	def initialize
+		get_oss_client
+		get_bucket
+	end
+
+	def get_oss_client
+		endpoint = ENV["CRAWLAB_OSS_ENDPOINT"]
+		access_key_id = ENV["CRAWLAB_OSS_ACCESS_KEY"]
+		access_key_secret = ENV["CRAWLAB_OSS_SECRET"]
+		bucket = ENV["CRAWLAB_OSS_BUCKET"]
+		if endpoint == nil || endpoint == ""
+			return
+		end
+		if access_key_id == nil || access_key_id == ""
+			return
+		end
+		if access_key_secret == nil || access_key_secret == ""
+			return
+		end
+
+		if bucket == nil || bucket == ""
+			return
+		end
+		@oss_client = Aliyun::OSS::Client.new(
+		  :endpoint => endpoint,
+		  :access_key_id => access_key_id,
+		:access_key_secret => access_key_secret)		
+	end
+
+	def get_bucket
+		bucket = ENV["CRAWLAB_OSS_BUCKET"]
+		if bucket == nil || bucket == ""
+			return
+		end
+		@bucket = @oss_client.get_bucket(bucket)
+	end
+
+	def send(oss_path,file_path)
+		if bucket == nil
+			return "bucket is nil"
+		end
+		bucket.put_object(oss_path, :file => self.file_path)
+	end
+end
