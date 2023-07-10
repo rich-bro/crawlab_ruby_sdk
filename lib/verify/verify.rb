7d71732a89d1c9ae9f3790614cd740c025529c82
李金敏 <lijinmin@socialdatamax.com>
2023-07-10 15:15:17 +0800

    m

--- lib/verify/verify.rb
+++ lib/verify/verify.rb
@@ -1,11 +1,15 @@
 class Verify
-def self.IsVerified?(itmes=[],table_name="")
+	def self.IsVerified?(itmes=[],table_name="")
 
-	if table_name == ""
-		
+		if table_name == "" && len(items) > 0
+			switch_table(items[0])
+		end
+
+		return true
 	end
 
-	return true
-end
+	def switch_table(item={})
+		puts $map_models
+	end
 
 end
